ea69aa1a142a9b0f8b51b1dff5aa85af07aaa226
音石朋恵 <tomoe_otoishi@Tomoe-Otoishi.local>
2017-02-12 14:26:30 +0900

    追加ファイル

---
+++ Instagram/Instagram-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Instagram-Bridging-Header.h
+//  Instagram
+//
+//  Created by 音石朋恵 on 2017/02/09.
+//  Copyright © 2017年 tomoe.otoishi. All rights reserved.
+//
+
+#ifndef Instagram_Bridging_Header_h
+#define Instagram_Bridging_Header_h
+
+#import <AdobeCreativeSDKCore/AdobeCreativeSDKCore.h>
+#import <AdobeCreativeSDKImage/AdobeCreativeSDKImage.h>
+
+#endif /* Instagram_Bridging_Header_h */
